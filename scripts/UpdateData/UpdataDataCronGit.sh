16c8ddb89caf0ac08737f120070568faa0fb4be5
Tyler Pollard <tylerpollard410@gmail.com>
2025-01-03 20:57:39 -0500

    cron jobs 2

--- scripts/UpdateData/UpdataDataCronGit.sh
+++ scripts/UpdateData/UpdataDataCronGit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Navigate to the local Git repository
-cd /path/to/your/local/repo
+cd /Users/tylerpollard/Desktop/NFLAnalysisTest
 
 # Add changes to the staging area
-git add .
+git add -A
 
 # Commit the changes with a timestamp message
 git commit -m "Auto-update: $(date +'%Y-%m-%d %H:%M:%S')"
 
 # Push the changes to the remote repository
-git push origin main
+git push
--- scripts/UpdateData/UpdateDataCron.R
+++ scripts/UpdateData/UpdateDataCron.R
@@ -2,18 +2,25 @@ library(cronR)
 
 cron_ls()
 
-path <- "/Users/tylerpollard/Desktop/NFLAnalysisTest/scripts/UpdateData/UpdateDataCron.R"
+path1 <- "/Users/tylerpollard/Desktop/NFLAnalysisTest/scripts/UpdateData/UpdateDataCronScript.R"
+path2 <- "/Users/tylerpollard/Desktop/NFLAnalysisTest/scripts/UpdateData/UpdateDataCronGit.sh"
 
-cmd <- cron_rscript(path)
+cmd1 <- cron_rscript(path1)
+cmd2 <- cron_rscript(path2)
 
-cron_add(command = cmd, 
+cron_add(command = cmd1, 
          frequency = "daily", 
-         at = "20:30", 
+         at = "21:00", 
          id = "test1",
-         description = "My process 1", 
-         tags = c("lab", "xyz")
-         )
+         description = "My process 1"
+)
+cron_add(command = cmd2, 
+         frequency = "daily", 
+         at = "21:02", 
+         id = "test2",
+         description = "My process 2"
+)
 
 cron_ls()
 
-cron_rm("test1")
+cron_rm("job_eca3e1bd13736aff8749d48be8b42aa4")
